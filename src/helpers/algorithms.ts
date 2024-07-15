e26bef044cca61be51e80b10510b5ae0fcabefaf
Jorge Rafael Rosero <rafaelrosero.r.r2408@gmail.com>
2024-07-15 10:18:22 -0500

    updating algorithm options

--- src/helpers/algorithms.ts
+++ src/helpers/algorithms.ts
@@ -1,8 +1,7 @@
 const algorithmsOptions = [
-    { label: 'Random Forest', value: 0 },
+    { label: 'Gradient Boosting', value: 0 },
     { label: 'Redes Neuronales', value: 1 },
     { label: 'Regresión Lineal', value: 2 },
-    { label: 'Regresión Logística', value: 3 },
 ]
 
 export default algorithmsOptions;
